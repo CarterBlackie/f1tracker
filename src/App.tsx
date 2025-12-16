ed337649e87f9199fbb0260c9268ca20199b443a
Carter Blackie <carterblackie55@gmail.com>
2025-12-16 11:08:59 -0500

    Initial Vite Setup
    Set up the default webpage to be working.

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ padding: "2rem", fontFamily: "system-ui" }}>
+      <h1>F1 Live Tracker</h1>
+      <p>Season hub coming soon.</p>
+    </div>
+  );
+}
+
+export default App;
